1445db54fb1fdad7cb52e706d248c3e4a706ed41
wuttichaimanonuke <wm_notstupid@hotmail.com>
2017-09-21 19:35:47 +0700

    Begin create project.

---
+++ src/main/java/com/gable/tech/srm/srmsoapservice/SrmSoapServiceApplication.java
@@ -0,0 +1,12 @@
+package com.gable.tech.srm.srmsoapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrmSoapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrmSoapServiceApplication.class, args);
+	}
+}
